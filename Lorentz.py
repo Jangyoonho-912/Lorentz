50da80df230e36923b2e8416876a7b201a043f6e
profyh <68215606+Jangyoonho-912@users.noreply.github.com>
2023-11-01 10:47:26 +0900

    Add files via upload

--- Lorentz.py
+++ Lorentz.py
@@ -23,27 +23,6 @@ V_d = 0
 series = V_x
 t_relax = 1e-3
 process_result_plot (V_d, series, t_relax)
-# %% make training set
-input_size = 10
-training_set_x, training_set_y, training_set_z = mk_Lr_training_set (t_relax, training_step,input_size)
-#x-axis
-training_set_0_x = np.array(training_set_x[0])
-training_set_1_x = np.array(training_set_x[1])
-training_set_2_x = np.array(training_set_x[2])
-#y-axis
-training_set_0_y = np.array(training_set_y[0])
-training_set_1_y = np.array(training_set_y[1])
-training_set_2_y = np.array(training_set_y[2])
-#z-axis
-training_set_0_z = np.array(training_set_z[0])
-training_set_1_z = np.array(training_set_z[1])
-training_set_2_z = np.array(training_set_z[2])
-# %% x-axis
-training_set = np.hstack((training_set_0_x, training_set_1_x, training_set_2_x))
-update = 40
-update_interval = 40
-test_step = 5
-y_tested, nrmse = Lr_training_test_update(training_set, training_step, test_step, t_relax, update, update_interval,input_size)
 
 # %% XYZ_integrated
 t_relax = 1e-3
